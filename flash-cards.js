5826d2dfe596c42d27221ce27833b36929384dab
ericb2020 <ebrauer83@gmail.com>
2021-01-08 18:31:03 -0600

    values assigned to cards

--- flash-cards.js
+++ flash-cards.js
@@ -1,17 +1,14 @@
-// PSEUDOCODE - BRONZE
-// create a 4 x 3 grid
-// populate each grid location with string data
-// make each grid location clickable
-// make clicking a grid location "reveal" that location
-// create an event that "hides" the revealed locations
-// allow for only 2 locations to be revealed before they must be hidden again
-// compare values of revlealed locations
-// if revealed locations match, hide them and remove them from play
-// continue until all matches have been found
-
 console.log("script file linked");
 
-////// GRID CREATION //////
+////// DATA FOR CARDS //////
+// have an array of data (1/2 the number of cards)
+let dataArray = ["red", "yellow", "green", "blue", "purple", "black"];
+
+// duplicate the data so that there are as many data points as cards
+// there will be 1/2 as many unique values, meaning every card will have a matching data point
+dataArray = dataArray.concat(dataArray);
+
+////// BOARD CREATION //////
 // setting number of rows and columns (programmer input)
 let columnInput = 4;
 let rowInput = 3;
@@ -19,10 +16,10 @@ let rowInput = 3;
 //converting rows and coluns to a string to add to template columns/rows CSS properties
 let columnString = ""
 let rowString = ""
-for (i=0; i<columnInput; i++) {
+for (let i=0; i<columnInput; i++) {
     columnString += "auto ";
 }
-for (i=0; i<rowInput; i++) {
+for (let i=0; i<rowInput; i++) {
     rowString += "auto ";
 }
 const columnSetting = columnString.slice(0, columnString.length-1);
@@ -39,26 +36,39 @@ grid.style.gridGap = "10px 10px"; // https://www.w3schools.com/cssref/tryit.asp?
 
 // create "cards"
 let numberOfCards = columnInput * rowInput;
-for (i=0; i<numberOfCards; i++) {
+for (let i=0; i<numberOfCards; i++) {
     let card = document.createElement("div");
     card.classList.add("card");
     card.setAttribute("id", `Card${i+1}`);
     grid.appendChild(card);
+    assignCards(i);
+}
+
+// assign the data to the "card" divs
+function assignCards(i) {
+    document.querySelector(`#Card${i+1}`).innerHTML = dataArray[i];
 }
 
 
-// grid.setAttribute("grid-template-columns", "auto")
 
-// const div1 = document.querySelector("#id1");
-// const div2 = document.querySelector("#id2");
-// const div3 = document.querySelector("#id3");
-// const div4 = document.querySelector("#id4");
 
-// div1.setAttribute("grid-column-start", "1");
-// div1.setAttribute("grid-column-end", "1");
-// div2.setAttribute("grid-column-start", "1");
-// div2.setAttribute("grid-column-end", "1");
-// div3.setAttribute("grid-column-start", "2");
-// div3.setAttribute("grid-column-end", "2");
-// div4.setAttribute("grid-column-start", "1");
-// div4.setAttribute("grid-column-end", "1");
+// cardPopulation();
+// let cardArray = [];
+// function cardPopulation(){
+//     for (let i=0; i<numberOfCards; i++){
+//         cardArray.push(`Card${i+1}`);
+//     }
+// }
+
+
+
+// PSEUDOCODE - BRONZE
+// create a 4 x 3 grid
+// populate each grid location with string data
+// make each grid location clickable
+// make clicking a grid location "reveal" that location
+// create an event that "hides" the revealed locations
+// allow for only 2 locations to be revealed before they must be hidden again
+// compare values of revlealed locations
+// if revealed locations match, hide them and remove them from play
+// continue until all matches have been found
