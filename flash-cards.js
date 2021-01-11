7463c23ff23022d17d8478e8882f5bf1109da68e
ericb2020 <ebrauer83@gmail.com>
2021-01-10 22:42:24 -0600

    added a reset button, does not yet have full functionality

--- flash-cards.js
+++ flash-cards.js
@@ -8,6 +8,7 @@ let dataArray = ["red", "yellow", "green", "blue", "purple", "black"];
 // there will be 1/2 as many unique values, meaning every card will have a matching data point
 dataArray = dataArray.concat(dataArray);
 
+
 ////// BOARD CREATION //////
 // setting number of rows and columns (programmer input)
 let columnInput = 4;
@@ -57,12 +58,33 @@ function assignCards(i) {
 
 let flipCount = 0;
 
+// add an event listener to every card
 for (let i = 0; i < numberOfCards; i++) {
+    const resetButton = document.querySelector("#reset");
+    resetButton.addEventListener("click", function (e) {
+    reset();
+    })
+
+    const reset = function() {
+        console.log("reset clicked");
+        for (let i = 0; i < numberOfCards; i++) {
+            document.querySelector(`#gridLocation${i+1}`).classList.remove("flippedCard", "matchedCards")
+            document.querySelector(`#card${i+1}`).classList.remove("visibleCard", "matchedCards")
+            document.querySelector(`#gridLocation${i+1}`).classList.add("gridLocation");
+            document.querySelector(`#card${i+1}`).classList.add("card");
+            console.log(i);
+        }
+    }
+
     const oneCard = document.querySelector(`#gridLocation${i+1}`);
+    const oneCard2 = document.querySelector(`#card${i+1}`);
     oneCard.addEventListener("click", function(){
+        // updates the flipCount only if the card has not already been flipped
         if (oneCard.classList.value == "gridLocation" && flipCount < 2) {
             oneCard.classList.replace("gridLocation", "flippedCard");
+            oneCard2.classList.replace("card", "visibleCard");
             flipCount++;
+            // waits for click anywhere on the page before running flipCheck            
             if (flipCount == 2) {
                 let holdClick = 0;
                 console.log("flipcheck");
@@ -77,54 +99,86 @@ for (let i = 0; i < numberOfCards; i++) {
         }
     })
 }
+
+// create array of cards to compare
 let cardCompare = [];
 
 
-// only flip two cards at a time
+// resets flipCount to zero, populates selected cards to cardCompare, then compares the values of the cards
 function flipCheck () {
     if (flipCount == 2) {
         flipCount = 0;
-        // document.body.addEventListener("click", function(e) {
-            console.log("reset cards");
-            for (let i = 0; i < numberOfCards; i++) {
-                const oneCard = document.querySelector(`#gridLocation${i+1}`);
-                
-                let cardTwo = NaN;
-                if (oneCard.classList.value == "flippedCard" && cardCompare[0] == null) {
-                    cardCompare.push(i);
-                    console.log(cardCompare);                 
-                } else if (oneCard.classList.value == "flippedCard" && cardCompare[0] >= 0) {
-                    cardCompare.push(i);
-                    console.log(cardCompare);
-                }
+        console.log("reset cards");
+        // populates selected cards to cardCompare by seeing which cards have the flippedCard class
+        for (let i = 0; i < numberOfCards; i++) {
+            const oneCard = document.querySelector(`#gridLocation${i+1}`);
+            if (oneCard.classList.value == "flippedCard" && cardCompare[0] == null) {
+                cardCompare.push(i);
+                console.log(cardCompare);                 
+            } else if (oneCard.classList.value == "flippedCard" && cardCompare[0] >= 0) {
+                cardCompare.push(i);
+                console.log(cardCompare);
             }
-            if (document.querySelector(`#card${cardCompare[0]+1}`).innerHTML == document.querySelector(`#card${cardCompare[1]+1}`).innerHTML) {
-                console.log(document.querySelector(`#card${cardCompare[0]+1}`).innerHTML);
-                console.log(document.querySelector(`#card${cardCompare[1]+1}`).innerHTML);
-                console.log("match");
-                document.querySelector(`#gridLocation${cardCompare[0]+1}`).classList.replace("flippedCard", "matchedCards");
-                document.querySelector(`#gridLocation${cardCompare[1]+1}`).classList.replace("flippedCard", "matchedCards");
-                cardCompare = [];
-            } else {
-                document.querySelector(`#gridLocation${cardCompare[0]+1}`).classList.replace("flippedCard", "gridLocation");
-                document.querySelector(`#gridLocation${cardCompare[1]+1}`).classList.replace("flippedCard", "gridLocation");    
-                cardCompare = [];
-            }
-        // })
+        }
+        // if cards match, class will changed to matchedCards, else the cards will be returned to gridLocation class (flipped back over)
+        if (document.querySelector(`#card${cardCompare[0]+1}`).innerHTML == document.querySelector(`#card${cardCompare[1]+1}`).innerHTML) {
+            console.log(document.querySelector(`#card${cardCompare[0]+1}`).innerHTML);
+            console.log(document.querySelector(`#card${cardCompare[1]+1}`).innerHTML);
+            console.log("match");
+            document.querySelector(`#gridLocation${cardCompare[0]+1}`).classList.replace("flippedCard", "matchedCards");
+            document.querySelector(`#gridLocation${cardCompare[1]+1}`).classList.replace("flippedCard", "matchedCards");
+            document.querySelector(`#card${cardCompare[0]+1}`).classList.replace("visibleCard", "matchedCards");
+            document.querySelector(`#card${cardCompare[1]+1}`).classList.replace("visibleCard", "matchedCards");
+            cardCompare = [];
+            matchMessage();
+            
+
+        } else {
+            document.querySelector(`#gridLocation${cardCompare[0]+1}`).classList.replace("flippedCard", "gridLocation");
+            document.querySelector(`#gridLocation${cardCompare[1]+1}`).classList.replace("flippedCard", "gridLocation");
+            document.querySelector(`#card${cardCompare[0]+1}`).classList.replace("visibleCard", "card");
+            document.querySelector(`#card${cardCompare[1]+1}`).classList.replace("visibleCard", "card");        
+            cardCompare = [];
+            noMatchMessage();
+        } 
     }
 }
 
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
 
-// cardPopulation();
-// let cardArray = [];
-// function cardPopulation(){
-//     for (let i=0; i<numberOfCards; i++){
-//         cardArray.push(`Card${i+1}`);
-//     }
-// }
+let totalMatchedCards = 0;
+///// why is the timer inconsistent? /////
+const matchMessage = function () {
+    document.querySelector("h1").innerHTML = "You got a match!! Keep going!";
+    for (let i = 0; i < numberOfCards; i++) {
+        if (document.querySelector(`#card${i+1}`).classList.value == "matchedCards") {
+            totalMatchedCards++;
+            console.log(totalMatchedCards);
+            if (totalMatchedCards == numberOfCards) {
+                console.log("game over, man!");
+                // sleep(500);
+                document.querySelector("h1").innerHTML = "YOU WIN!";
+                document.querySelector("h2").innerHTML = "(hit \"reset\" to play again)";
+            }
+        }
+    }
+    totalMatchedCards = 0;
+}
 
+const noMatchMessage = function () {
+    document.querySelector("h1").innerHTML = "No match. Try Again.";
+}
 
-
+const clearMessage = function() {
+    sleep(2000);
+    document.querySelector("h1").innerHTML = "Top again.";
+}
 // PSEUDOCODE - BRONZE
 // create a 4 x 3 grid
 // populate each grid location with string data
